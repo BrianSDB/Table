0d92efab6d36fa82f2bb89be66bfc51cd5d1bd66
Brian <118107819+BrianSDB@users.noreply.github.com>
2023-06-20 17:33:19 +0100

    Commit

--- geonamesExample/libs/js/script.js
+++ geonamesExample/libs/js/script.js
@@ -1,85 +1,96 @@
 
 
 $('#weatherBtn').click(function(e){
-    e.preventDefault();
-    //alert("it works")
-    
-    $.ajax({
-      url:"geonamesExample/libs/php/weatherInfo.php",
-      type:"GET",
-      dataType: 'json',
-      data:JSON.stringify({  }),
-
-      success:function(result){
-        console.log(result);
-
-        if(result.status.name=="ok") {
-          $("#result").html(result.data.temperature),
-          $("#result").html(result.data.clouds),
-          $("#result").html(result.data.lng),
-          $("#result").html(result.data.lat)
-        }
-        
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-				// your error code
-        console.log(textStatus)
-			}
-    })
-  });
-
+  e.preventDefault();
+  //alert("it works")
   
-  
+  $.ajax({
+    url:"geonamesExample/libs/php/weatherInfo.php",
+    type:"POST",
+    dataType: 'json',
+    data: {
+      stationName:$('#stationName').val(),
+    },
 
+    success:function(result){
+      console.log(result);
+      $('#result').html('');
 
-
-
-  $("#timezoneBtn").click(function(e){
-    e.preventDefault();
-    //alert("it works")
-    $.ajax({
-      url:"geonamesExample/libs/php/timezoneInfo.php",
-      type:"GET",
-      dataType:"json",
-      data: JSON.stringify({  }),
-
-      success:function(result){
-        console.log(result);
-          $('#result').append(result.data.sunrise);
-          $('#result').append(result.data.sunset);
-          $('#result').append(result.data.countryName);
-        
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-				// your error code
-        console.log(textStatus)
-			}
-    })
-  });
-
-  $("#elevationBtn").click(function(e){
-    e.preventDefault();
-    $.ajax({
-      url:"geonamesExample/libs/php/elevationInfo.php",
-      type:"GET",
-      dataType:"json",
-      data:JSON.stringify({  }),
-      success:function(result){
-        console.log(result);
-
-        if(result.status.name =="ok") {
-          $('#result').html(result.data.lng);
-          $('#result').html(result.data.astergdem);
-          $('#result').html(result.data.lat);
-        }
-        
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        // your error code
-        console.log(textStatus)
+      $('#result').append('Temperature: '+result.data.weatherObservations['0'].temperature + '<br/>');
+      $('#result').append('Humidity: '+result.data.weatherObservations['0'].humidity + '<br/>');
+      $('#result').append('Station Name: '+result.data.weatherObservations['0'].stationName + '<br/>');
+      $('#result').append('Longitude: '+result.data.weatherObservations['0'].lng + '<br/>');
+      $('#result').append('Latitude: '+result.data.weatherObservations['0'].lat + '<br/>');
+      
+      
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      // your error code
+      console.log(textStatus)
     }
-    })
   })
+});
+
+
+
+
+
+
+
+$("#timezoneBtn").click(function(e){
+  e.preventDefault();
+  //alert("it works")
+  $.ajax({
+    url:"geonamesExample/libs/php/timezoneInfo.php",
+    type:"POST",
+    dataType:"json",
+    data: JSON.stringify({ }),
+
+    success:function(result){
+      console.log(result);
+        $('#result').html('');
+
+
+        $('#result').append('Sunrise:'+result.data.sunrise + '<br/>');
+        $('#result').append('Sunset:'+result.data.sunset + '<br/>');
+        $('#result').append('Timezone:'+result.data.timezoneId + '<br/>');
+        $('#result').append('Country:'+result.data.countryName + '<br/>');
+      
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      // your error code
+      console.log(textStatus)
+    }
+  })
+});
+
+$("#elevationBtn").click(function(e){
+  e.preventDefault();
+  $.ajax({
+    url:"geonamesExample/libs/php/elevationInfo.php",
+    type:"GET",
+    dataType:"json",
+    data:{
+      lng:$('#lng').val(),
+      lat:$('#lat').val()
+    },
+    success:function(result){
+      console.log(result);
+        $('#result').html('');
+      
+        $('#result').append('Longitude: '+result.data.lng + '<br/>');
+        $('#result').append('Elevation: '+result.data.astergdem + '<br/>');
+        $('#result').append('Latitude: '+result.data.lat + '<br/>');
+      
+      
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      // your error code
+      console.log(textStatus)
+  }
+  })
+})
+
 
 
 
